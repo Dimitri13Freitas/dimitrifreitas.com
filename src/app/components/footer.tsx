4fa6065e634778401d37a682dd2bfd3211ec6b99
Dimitri13Freitas <freitasdimitri7@gmail.com>
2025-05-03 23:51:04 -0300

    start footer component

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 h-[100px] w-[40px] bg-gray-800 text-white flex items-center justify-center">
+      <span className="-rotate-90 whitespace-nowrap text-sm">
+        © 2025 Dimitri Freitas
+      </span>
+    </footer>
+  );
+}
