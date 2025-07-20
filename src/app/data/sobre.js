a2da6568eb1f5f5639ac962236df73be0cda393b
Dimitri13Freitas <freitasdimitri7@gmail.com>
2025-07-20 16:34:08 -0300

    test a different version of tailwind

--- src/app/data/sobre.js
+++ src/app/data/sobre.js
@@ -7,6 +7,14 @@ const carrerJobs = [
     endDate: "",
     location: "São Roque, SP",
   },
+    {
+    jobTitle: "Assistente de TI",
+    company: "DR Construtora",
+    companyUrl: "https://grupodr.com.br/gr",
+    startDate: "2023-03-11",
+    endDate: "",
+    location: "São Roque, SP",
+  },
 ];
 
 export { carrerJobs };
