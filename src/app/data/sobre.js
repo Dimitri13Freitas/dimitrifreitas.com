96384cc4087212d80f9506766a24e661a80acea2
Dimitri13Freitas <freitasdimitri7@gmail.com>
2025-08-06 21:53:42 -0300

    revert: restore a working version of the project

--- src/app/data/sobre.js
+++ src/app/data/sobre.js
@@ -7,14 +7,6 @@ const carrerJobs = [
     endDate: "",
     location: "São Roque, SP",
   },
-    {
-    jobTitle: "Assistente de TI",
-    company: "DR Construtora",
-    companyUrl: "https://grupodr.com.br/gr",
-    startDate: "2023-03-11",
-    endDate: "",
-    location: "São Roque, SP",
-  },
 ];
 
 export { carrerJobs };
